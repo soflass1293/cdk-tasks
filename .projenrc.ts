5f013f92c12cc9cec8c2855bbc6dcd350830131c
Sofienne Lassoued <soflass1293@gmail.com>
2024-05-25 17:14:59 +0100

    Initialized backend

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,10 +1,17 @@
-import { typescript } from "projen";
+import { typescript, awscdk } from "projen";
 import { monorepo } from "@aws/pdk";
 const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
   name: "cdk-tasks",
   projenrcTs: true,
 });
+new awscdk.AwsCdkTypeScriptApp({
+  name: "backend",
+  defaultReleaseBranch: "main",
+  cdkVersion: "2.142.1",
+  parent: project,
+  outdir: "backend",
+})
 const frontend = new typescript.TypeScriptAppProject({
   name: "frontend",
   defaultReleaseBranch: "main",
