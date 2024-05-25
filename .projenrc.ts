08f34c8d3fac1f40b6a9145fb6c85c90df3f8d29
Sofienne Lassoued <soflass1293@gmail.com>
2024-05-25 14:08:47 +0100

    Added frontend

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,22 @@
+import { typescript } from "projen";
 import { monorepo } from "@aws/pdk";
 const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
   name: "cdk-tasks",
   projenrcTs: true,
+});
+const frontend = new typescript.TypeScriptAppProject({
+  name: "frontend",
+  defaultReleaseBranch: "main",
+  deps: ["alpinejs"],
+  devDeps: ["vite", "@types/alpinejs", "@types/web"],
+  outdir: "frontend",
+  parent: project,
+});
+frontend.addTask("dev", {
+  exec: "vite",
+});
+project.addTask("dev", {
+  exec: "APP_STAGE=dev npx projen run-many --all --targets=dev",
 });
 project.synth();
---
+++ frontend/src/main.ts
@@ -0,0 +1,6 @@
+import Alpine from 'alpinejs'
+
+// @ts-ignore
+window.Alpine = Alpine
+ 
+Alpine.start()
