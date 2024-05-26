d1b55886c66f78545da723f37429a91cccec7746
Sofienne Lassoued <soflass1293@gmail.com>
2024-05-26 20:53:07 +0100

    ppp

--- backend/src/bin/cdk-todos.ts
+++ backend/src/bin/cdk-todos.ts
@@ -1,27 +1,31 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { APIStack, DatabaseStack, EventsStack, WebsiteStack } from '../lib';
-import 'dotenv/config';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { APIStack, DatabaseStack, EventsStack, WebsiteStack } from "../lib";
+import "dotenv/config";
 
-const stage = process.env.STAGE;
+function createStackName(stackName: string) {
+  const stage = process.env.STAGE;
+  const app_name = process.env.MY_APP || "tasks";
+  return `${app_name}-${stackName}-${stage}`;
+}
 
 const app = new cdk.App();
 
-const { table, archivedTable } = new DatabaseStack(app, 'DatabaseStack', {
-  stackName: `database-${stage}`,
+const { table, archivedTable } = new DatabaseStack(app, "DatabaseStack", {
+  stackName: createStackName("database"),
 });
-const api = new APIStack(app, 'APIStack', {
+const api = new APIStack(app, "APIStack", {
   table,
   archivedTable,
-  stackName: `api-${stage}`,
+  stackName: createStackName("api"),
 });
-new EventsStack(app, 'EventsStack', {
+new EventsStack(app, "EventsStack", {
   archivedTable,
-  stackName: `events-${stage}`,
+  stackName: createStackName("events"),
 });
-new WebsiteStack(app, 'WebsiteStack', {
-  stackName: `website-${stage}`,
+new WebsiteStack(app, "WebsiteStack", {
+  stackName: createStackName("website"),
   graphqlUrl: api.graphqlUrl,
   apiKey: api.apiKey,
 });
