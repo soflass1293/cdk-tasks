3b8114dd9d57b400599b3032174496a2f831d781
Sofienne Lassoued <soflass1293@gmail.com>
2024-05-26 19:20:17 +0100

    update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,12 @@
-import Alpine from 'alpinejs';
-import './app';
+import Alpine from "alpinejs";
+import "./app";
 // @ts-ignore
 window.Alpine = Alpine;
 Alpine.start();
+
+// @ts-ignore
+const HTTP_LINK = import.meta.env.VITE_API_HOST;
+// @ts-ignore
+const API_KEY = import.meta.env.VITE_API_KEY;
+console.log(HTTP_LINK);
+console.log(API_KEY);
