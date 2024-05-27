1558bc0df56cd2b2c103cefcf2cfcbbdb270e95a
Sofienne Lassoued <soflass1293@gmail.com>
2024-05-27 17:47:32 +0100

    update

--- backend/src/bin/cdk-todos.ts
+++ backend/src/bin/cdk-todos.ts
@@ -3,11 +3,12 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { APIStack, DatabaseStack, EventsStack, WebsiteStack } from "../lib";
 import "dotenv/config";
+import { genstr } from "../lib/utils/rand-str";
 
 function createStackName(stackName: string) {
   const stage = process.env.STAGE || "ephemeral";
   const app_name = process.env.MY_APP || "tasks";
-  return `${app_name}-${stackName}-${stage}`;
+  return `${app_name}-${stackName}-${stage}-${genstr(5)}`;
 }
 
 const app = new cdk.App();
