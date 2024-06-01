f07b514753b29fdf0cae6789769639a2c2aefa22
soflass1293 <soflass1293@gmail.com>
2024-06-01 14:01:15 +0100

    Test ci cd workflow (#18)
    * few changes
    
    * delete main workflow

--- backend/src/lib/stacks/events/workers/archive-todo.ts
+++ backend/src/lib/stacks/events/workers/archive-todo.ts
@@ -1,7 +1,5 @@
-import { EventBridge } from '@aws-sdk/client-eventbridge';
 import { DynamoDBAdapter } from '../../database/adapters';
 import { ArchiveTodoCommand } from '../../database/commands/archive-todo-command';
-import { EventDispatcher } from '../dispatchers/event-dispatcher';
 import { TodoVotedEventEventBridgeEvent } from '../records/todo-deleted-event';
 
 const archiveTodoCommand = new ArchiveTodoCommand({
